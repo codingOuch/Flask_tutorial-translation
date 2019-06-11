752a424f5d251bc9f2d758a386e756558653590e
haowen zhu <dearzhw@gmail.com>
2019-06-11 18:03:40 +0800

    Learned render template and web-form

--- Chapter I Hello, world!/app.py
+++ Chapter I Hello, world!/app.py
@@ -1,8 +1,35 @@
 from flask import Flask
+from flask import render_template, flash, redirect
+from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+@app.route('/index')
+def index():
+    user = {'name': 'Howard'}
+    posts = [{
+                'author': {'user': 'John'},
+                  'post': 'This is my first post!'
+              },
+             {
+                 'author': {'user': 'Howard'},
+                  'post': 'I am the king of coding!'
+             }]
+    return render_template('index.html', title='', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
