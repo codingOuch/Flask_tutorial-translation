90e00a0981897513ee08cbd9e0c93e5aa96cbae8
haowen zhu <dearzhw@gmail.com>
2019-06-12 17:42:33 +0800

    Learned render template and web-form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from config import Config
-from forms import LoginForm
+from forms import LoginForm, RegisterForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -26,9 +26,19 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Login', form=form)
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    confirm = PasswordField('Confirm password', validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Submit')
